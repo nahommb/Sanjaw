1c060ca663a8b0646352130211bcee685b81ef4e
nahom <nahomjr17@gmail.com>
2025-11-11 10:44:03 -0600

    update

--- lib/features/home/presentation/provider/story_provider.dart
+++ lib/features/home/presentation/provider/story_provider.dart
@@ -4,6 +4,19 @@ import 'package:sanjaw/features/home/data/models/story_model.dart';
 import 'package:sanjaw/features/home/data/repositories/story_repository.dart';
 
 
-final StoryRepositoryProvider = Provider((ref)=>StoryRepository());
+final storyRepositoryProvider = Provider((ref)=>StoryRepository());
 
-final storyListProvider = StateNotifierProvider<StoryNotifier,AsyncValue<StoryModel>>
+final storyListProvider = StateNotifierProvider<StoryNotifier,AsyncValue<List<StoryModel>>>((ref){
+
+  final repository = ref.read(storyRepositoryProvider);
+
+  return StoryNotifier(repository);
+}
+);
+
+
+class StoryNotifier extends StateNotifier<AsyncValue<List<StoryModel>>>{
+
+  StoryNotifier(StoryRepository repository):super(AsyncValue.loading());
+
+}
