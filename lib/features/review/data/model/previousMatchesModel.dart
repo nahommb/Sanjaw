95f86b6242bc52af245b95e6a0d007dd9e5f2c51
nahom <nahomjr17@gmail.com>
2025-11-14 11:06:12 -0600

    updated

---
+++ lib/features/review/data/model/previousMatchesModel.dart
@@ -0,0 +1,16 @@
+
+class Previousmatchesmodel {
+
+  final String homeTeam;
+  final String awayTeam;
+  final String homeScore;
+  final String awayScore;
+
+ Previousmatchesmodel({
+  required this.homeTeam,
+  required this.awayTeam,
+  required this.homeScore,
+  required this.awayScore
+ });
+ 
+}
--- lib/features/review/presentation/review_screen.dart
+++ lib/features/review/presentation/review_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sanjaw/core/config/app_colors.dart';
 
 class ReviewScreen extends StatelessWidget {
 
@@ -9,13 +10,56 @@ class ReviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-            child: Text('Hellooooooo'),
-          )
-        ],
-      ),
+      body: ListView.builder(
+        itemCount: 15,
+        itemBuilder: (context,index){
+      
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 10,),
+            Text('Ethiopian Cup',style: TextStyle(fontWeight: FontWeight.bold),),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 20,),
+              margin: EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                border:Border(
+                  left: BorderSide(
+                    color: AppColors.secondaryColor,
+                    width: 6
+                  ),
+                  right: BorderSide(
+                    color: AppColors.primaryColor,
+                    width: 6
+                  )
+                )
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text('Ethiopian Coffee',style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text('2'),
+                  Text('vs'),
+                  Text('2'),
+                  Text('St George',style: TextStyle(fontWeight: FontWeight.bold),),
+                  
+                ],
+              ),
+            ),
+            // Padding(
+            // padding: EdgeInsetsGeometry.all(8),
+            // child: Text('Vs'),
+            // ),
+            // Row(
+            //   children: [
+            //     Text('St George'),
+            //     Text('2')
+            //   ],
+            // ),
+          ],
+        );
+      }),
+      
     );
   }
 }
